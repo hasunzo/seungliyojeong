466088c6e4fa90739834aabb2aa2aafde2515870
hasunzo <gktjsdl470@gmail.com>
2023-06-11 11:17:40 +0900

    feat 빌드 배포 구성

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,18 @@ plugins {
 	val kotlinVersion = "1.7.20"
 	id("org.springframework.boot") version "3.1.0"
 	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version kotlinVersion
-	kotlin("plugin.spring") version kotlinVersion
+	id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
 	kotlin("plugin.jpa") version kotlinVersion
+	kotlin("plugin.spring") version kotlinVersion
+	kotlin("jvm") version kotlinVersion
+}
+
+allOpen {
+	annotation("jakarta.persistence.Entity")
+}
+
+noArg {
+	annotation("jakarta.persistence.Entity")
 }
 
 group = "com.server.slyj"
